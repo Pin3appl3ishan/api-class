7534ecc475ce43f83592edcc71c4e3764810e263
Pin3appl3ishan <sthaishan2004@gmail.com>
2025-05-27 22:45:42 +0545

    Add Product model with schema for productName, productPrice, productDescription, categoryId, and sellerId

---
+++ models/Product.js
@@ -0,0 +1,27 @@
+const mongoose = require('mongoose');
+const ProductSchema = new mongoose.Schema({
+  productName: {
+    type: String,
+    required: true,
+  },
+  productPrice: {
+    type: Number,
+    required: true,
+  },
+  productDescription: {
+    type: String,
+    required: true,
+  },
+  categoryId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category',
+    required: true,
+  },
+  sellerId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
